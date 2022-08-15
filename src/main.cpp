67df2470dd4b34fe2ba93504f1c9454d8bee1024
yumcyawiz <hashdolf1923@gmail.com>
2022-08-15 10:27:40 +0900

    update .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,10 @@
-int main() { return 0; }
+#include <cstdio>
+
+int main(int argc, char **argv) {
+  if (argc != 4) {
+    printf("usage: ./main color.hdr albedo.hdr normal.hdr\n");
+    return 0;
+  }
+
+  return 0;
+}
